d0e43a756e6db83a9ffd79df7d6b05655252fb5d
Igor Chernyak <i_chernyak@inbox.ru>
2021-03-28 15:25:05 +0300

    first commit

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12 lg6>
+        <v-menu v-if="this.range"
+          v-model="menu"
+          :close-on-content-click="false"
+          max-width="290"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              :value="dateFromFormatted"
+              clearable
+              label="From date"
+              readonly
+              v-on="on"
+            >
+            </v-text-field>
+            <v-text-field
+              :value="dateToFormatted"
+              clearable
+              label="To date"
+              readonly
+              v-on="on"
+              append-icon="mdi-calendar"
+            >
+            </v-text-field>
+          </template>
+          <v-date-picker
+            v-model="rangeDates"
+            range
+            @change="menu = false"
+            first-day-of-week="1"
+            header-color="#1E1E1E"
+          ></v-date-picker>
+        </v-menu>
+        <v-menu v-else
+          v-model="menu"
+          :close-on-content-click="false"
+          max-width="290"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              :value="singleDateFormatted"
+              clearable
+              label="Date"
+              readonly
+              append-icon="mdi-calendar"
+              v-on="on"
+            >
+            </v-text-field>
+          </template>
+          <v-date-picker
+            v-model="singleDate"
+            @change="menu = false"
+            first-day-of-week="1"
+            header-color="#1E1E1E"
+          ></v-date-picker>
+        </v-menu>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  name: 'DatePicker',
+  props: {
+    range: {
+      type: Boolean,
+      required: false
+    }
+  },
+  data: () => ({
+    singleDate: new Date().toISOString().substr(0, 10),
+    rangeDates: [
+      new Date().toISOString().substr(0, 10),
+      new Date().toISOString().substr(0, 10)
+    ],
+    menu: false
+  }),
+  computed: {
+    singleDateFormatted () {
+      return moment(this.singleDate).format('DD/MM/YYYY')
+    },
+    dateFromFormatted () {
+      return moment(this.rangeDates[0]).format('DD/MM/YYYY')
+    },
+    dateToFormatted () {
+      return moment(this.rangeDates[1]).format('DD/MM/YYYY')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container.grid-list-md .layout .flex {
+  display: flex;
+  justify-content: space-between;
+}
+.v-input {
+  max-width: 45%;
+}
+</style>
